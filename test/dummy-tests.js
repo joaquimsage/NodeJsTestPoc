44e1938581542244a4c1537713cbbb1cd9c0a515
Quino <joaquim.forcada@sage.com>
2017-09-19 18:48:53 +0200

    Adding more tests within another branch

--- test/dummy-tests.js
+++ test/dummy-tests.js
@@ -21,7 +21,14 @@ describe('Dummy tests', () => {
 
 
     it('Should locate position 3', function () {
-        assert.equal(Math.floor(Math.random()*(3-1+1)+1), [1, 2, 3].indexOf(3), 'Sometimes this assertion fails, maybe has sth to do with the Random number?!');
+        //assert.equal(Math.floor(Math.random()*(3-1+1)+1), [1, 2, 3].indexOf(3), 'Sometimes this assertion fails, maybe has sth to do with the Random number?!');
+        assert.ok('Empty test', 'Assert test is executed.');
+    });
+
+    it('Hello and Bye Words', function () {
+        //assert.equal(Math.floor(Math.random()*(3-1+1)+1), [1, 2, 3].indexOf(3), 'Sometimes this assertion fails, maybe has sth to do with the Random number?!');
+        assert.equal('Bye', 'Bye', 'These words are not the same and they should be.');
+        assert.notEqual('Hello', 'Bye', 'Words should not be euqal...and they are mf!');
     });
 
 });
