9617221e6c02adb4c927a638938e37fef4ca2f39
Quino <joaquim.forcada@sage.com>
2017-09-20 09:20:58 +0200

    Comment done() x 5

--- test/index.js
+++ test/index.js
@@ -1,26 +1,24 @@
-//var builder = require('botbuilder');
+var builder = require('botbuilder');
 
 var assert = require('assert');
 
-//var common = require('./common');
-//var luisMock = require('./luis-mock');
-//var testBot = require('../bot');
-//var helpMessages = require('./dialog-flows/help');
-//var setAlarmMessages = require('./dialog-flows/set-alarm');
-//var setAlarmMessages2 = require('./dialog-flows/set-alarm2');
+var common = require('./common');
+var luisMock = require('./luis-mock');
+var testBot = require('../bot');
+var helpMessages = require('./dialog-flows/help');
+var setAlarmMessages = require('./dialog-flows/set-alarm');
+var setAlarmMessages2 = require('./dialog-flows/set-alarm2');
 
-//luisMock.setup();
+luisMock.setup();
 
 //Our parent block
 describe('Bot Tests', function() {
 
   it('help', function (done) { 
-     // var connector = new builder.ConsoleConnector();
-      //var bot = testBot.create(connector);
+      var connector = new builder.ConsoleConnector();
+      var bot = testBot.create(connector);
 
-      //common.testBot(bot, helpMessages, done);
-      done();
-      assert.ok('Bot Builder is Missing, still missing mf');
+      common.testBot(bot, helpMessages, done);
   });
 
 });
