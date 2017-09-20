27f49ccb1186c8d0721d22d57a1aff61fb615fd0
Quino <joaquim.forcada@sage.com>
2017-09-20 13:23:16 +0200

    Comment done() x 7

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ function create(connector) {
       }
   ]);
 
-  dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+  dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I am not able to create alarms anymore."));
 
   // Very simple alarm scheduler
   var alarms = {};
